763583bae0d66fbf432f736e1421577abe9129ee
Nithin Mathew <nithin.mathew@sysfore.com>
2024-10-28 14:32:25 +0530

    Conversions and ElementOperations completed

--- Program.cs
+++ Program.cs
@@ -1,8 +1,25 @@
 ﻿#region AggregateOperations
 
 using LINQForBeginners.LINQ.AggregateOperations;
+using LINQForBeginners.LINQ.Conversions;
+using LINQForBeginners.LINQ.ElementOperations;
 
 AggregateOperators aggregateOperators = new AggregateOperators();
-aggregateOperators.SeededAggregate();
+//aggregateOperators.SeededAggregate();
+
+#endregion
+
+#region Conversions
+
+Conversions conversions = new Conversions();
+//conversions.ConvertSelectedItems();
+
+#endregion
+
+
+#region ElementOperations
+
+ElementOperations elementOperations = new ElementOperations();
+elementOperations.ElementAtPosition();
 
 #endregion
